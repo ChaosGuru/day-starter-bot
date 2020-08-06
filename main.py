601688e68fafb262bb54bae1479eb52626355fc5
ChaosGuru <chaos.feature@gmail.com>
2020-08-06 15:02:47 +0300

    try to deploy main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import json
-from datetime import datetime, time, timezone, timedelta
+from datetime import time, timezone, timedelta
 
 import telegram
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
@@ -12,14 +12,21 @@ def start(update, context):
     update.message.reply_text('♥Просто насолоджуйсь сьогоднішнім днем.\n\n♦Якщо також хочеш бота, або маєш пропозиції щодо цього напиши мені\n>>> https://t.me/I_NANI_I\n\n‼ /stop - зупинити бота', disable_web_page_preview=True)
 
     # set time
-    t = time(hour=7, tzinfo=timezone(timedelta(hours=3)))
+    t = time(hour=14, minute=47, tzinfo=timezone(timedelta(hours=3)))
     # t = time(hour=11, minute=30)
 
     # add a job
     if 'morning' not in context.chat_data:
         job = context.job_queue.run_daily(morning, t, context=update.message.chat_id)
-
         context.chat_data['morning'] = job
+
+    # add test job
+    if 'test' in context.chat_data:
+        context.chat_data['test'].schedule_removal()
+        del context.chat_data['test']
+    
+    test_job = context.job_queue.run_once(morning, 3, context=update.message.chat_id)
+    context.chat_data['test'] = test_job
 
 def stop(update, context):
     if 'morning' in context.chat_data:
@@ -33,8 +40,12 @@ def echo(update, context):
     update.message.reply_text(update.message.text)
 
 def morning(context):
+    logging.info("Morning job run.")
+
     job = context.job
     text = "Доброго ранку!\n\n"
+
+    # date
 
     # whether
 
@@ -69,3 +80,6 @@ def main(request=None):
 
 if __name__ == '__main__':
     main()
+
+# TODO: add job serilization https://github.com/python-telegram-bot/python-telegram-bot/wiki/Code-snippets#save-and-load-jobs-using-pickle
+#       finish morning job
